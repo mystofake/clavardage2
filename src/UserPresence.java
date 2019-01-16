bbba34b7bf85dc01f4c3c45a7f948e5bf5994e4d
Evan TISSOT <evan.tissot@gmail.com>
2019-01-16 18:53:02 +0100

    Servlet

---
+++ src/UserPresence.java
@@ -0,0 +1,25 @@
+import java.io.Serializable;
+import java.net.*;
+
+public class UserPresence implements Serializable {
+	private String userName;
+	private boolean onlineState;
+	private InetAddress addr;
+	
+	public UserPresence(String userName, InetAddress addr)
+	{
+		this.userName = userName;
+		this.addr = addr;
+		this.onlineState = true;
+	}
+	
+	public String getUserName()
+	{
+		return this.userName;
+	}
+	
+	public String toString()
+	{
+		return this.userName + "@"+ this.addr.toString();
+	}
+}
